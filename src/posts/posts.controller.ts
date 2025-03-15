7d8010462db57fa67ac8f37ea1149f10fdb7214a
Supriyo Mahanta <mahantasupriyo@gmail.com>
2025-03-15 19:26:07 +0530

    post controllers & routed added

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts(): string {
+    // TODO
+    return 'get all posts';
+  }
+
+  @Get('/:postid')
+  getPostDetails(@Param() param): string {
+    // TODO
+    return `details of postid = ${param.postid}`;
+  }
+
+  @Post('/')
+  createNewPost(): string {
+    // TODO
+    return `new post created`;
+  }
+
+  @Delete('/:postid')
+  deletePost(@Param() param): string {
+    // TODO
+    return `delete postID = ${param.postid}`;
+  }
+
+  @Put('/:postid/like')
+  likePost(@Param() param): string {
+    // TODO
+    return `liked post ${param.postid}`
+  }
+
+  @Delete('/:postid/unlike')
+  unlikePost(@Param() param): string {
+    // TODO
+    return `unliked post ${param.postid}`
+  }
+}
